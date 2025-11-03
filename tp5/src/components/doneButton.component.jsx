a6ac372fbf78f72fa8db828d3604df61d89ca1e6
SolennMarcot <solenn.marcot@gmail.com>
2025-11-03 19:49:31 +0100

    Javascript

--- tp5/src/components/doneButton.component.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const DoneButton = ({id,changeTasks}) => {
-    const handleClick = () => changeTasks(id);
-    return <div className="doneButton" onClick={handleClick}>V</div>;
-};
-export default DoneButton;
--- tp5/src/components/task.component.jsx
+++
@@ -1,8 +0,0 @@
-import PriorityScale from "./priorityScale.component.jsx";
-import DoneButton from "./doneButton.component.jsx";
-import React from "react";
-
-const Task = ({props, changeTasks, updateTaskPriority}) => {
-    return <div className = "task" key = {props.id} duration={props.duration}>{props.description}({props.duration}mn) <PriorityScale priority={props.priority} onPriorityChange={(newPriority) => updateTaskPriority(props.id, newPriority)}/> <DoneButton id={props.id} changeTasks={changeTasks}/></div>;
-}
-export default Task;
